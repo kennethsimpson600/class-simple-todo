f43ce380ffeb7df45ae71913e5d1c1e12a97c727
knight thompson <knightshell301@gmail.com>
2018-09-11 15:18:17 +0000

    Add event handlers for Task buttons

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -7,7 +7,6 @@ import './task.html';
 Template.task.events({
 
   'click .toggle-checked'() {
-
     // Set the checked property to the opposite of its current value
  	Tasks.update(this._id, {
       $set: { checked: ! this.checked },
